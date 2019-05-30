c9a978d64574c9362a90d4a99539252ac43fb636
Joshua Crum <joshua.crum@gmail.com>
2019-05-30 01:12:32 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,16 @@
 require_relative './fancy_dance.rb'
 
 class Kid
+
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
 end
